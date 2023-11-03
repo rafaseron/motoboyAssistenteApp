c08f77995922db9a00cf925b1874bd07c4838e86
rafaseron <rafaelseron3011@gmail.com>
2023-11-03 00:34:43 -0400

    var kotlin added

--- app/src/main/java/br/com/rafaseron/motoboyassistente/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/motoboyassistente/MainActivity.kt
@@ -2,10 +2,25 @@ package br.com.rafaseron.motoboyassistente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    //iniciando as variaveis que usaremos
+    lateinit var ifoodTelefone : EditText
+    lateinit var ifoodID : EditText
+    lateinit var telefoneDDI : EditText
+    lateinit var telefone : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun setupViews(){
+            ifoodTelefone = findViewById(R.id.edtIfood_telefone)
+            ifoodID = findViewById(R.id.edtIfood_ID)
+            telefoneDDI = findViewById(R.id.edtTelefone_DDI)
+            telefone = findViewById(R.id.edtTelefone_numero)
+        }
+
+
     }
 }
