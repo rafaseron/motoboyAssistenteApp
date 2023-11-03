35df26f76cff85f63b7f2bf5f2ac38f9403080ab
rafaseron <rafaelseron3011@gmail.com>
2023-11-03 01:31:07 -0400

    quase retornando certo

--- app/src/main/java/br/com/rafaseron/motoboyassistente/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/motoboyassistente/MainActivity.kt
@@ -2,7 +2,9 @@ package br.com.rafaseron.motoboyassistente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     //iniciando as variaveis que usaremos
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var ifoodID : EditText
     lateinit var telefoneDDI : EditText
     lateinit var telefone : EditText
+
+    //botoes para setOnClickListener
+    lateinit var btnIfood : Button
+    lateinit var btnWhatsapp : Button
+
+    //TextView para voltar resultados
+    lateinit var txtWhatsapp : TextView
+    lateinit var txtIfood : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,18 +29,32 @@ class MainActivity : AppCompatActivity() {
             ifoodID = findViewById(R.id.edtIfood_ID)
             telefoneDDI = findViewById(R.id.edtTelefone_DDI)
             telefone = findViewById(R.id.edtTelefone_numero)
+            btnIfood = findViewById(R.id.btnIfoodLigar)
+            btnWhatsapp = findViewById(R.id.btnMensagem)
+            txtWhatsapp = findViewById(R.id.txtWhatsappCompleto)
+            txtIfood = findViewById(R.id.txtIfoodTelefoneCompleto)
         }
         setupViews()
 
         fun ifoodCompleto(){
-            val telefoneIfoodCompleto = (ifoodTelefone.toString())+";"+(ifoodID.toString())
+            val telefoneIfoodCompleto = "0800"+(ifoodTelefone.toString())+";"+(ifoodID.toString())
+            txtIfood.text = telefoneIfoodCompleto
         }
-        ifoodCompleto()
 
         fun whatsappCompleto(){
             val telefoneWhatsappCompleto = "wa.me/"+(telefoneDDI.toString())+(telefone.toString())
+            txtWhatsapp.text = telefoneWhatsappCompleto
+
         }
-        whatsappCompleto()
+
+        fun setupListeners(){
+            btnIfood.setOnClickListener(){ifoodCompleto()
+            }
+            btnWhatsapp.setOnClickListener(){whatsappCompleto()
+            }
+
+        }
+        setupListeners()
 
 
     }
